f4689042651ea1cc322a21bed2e3ad340d6fc9af
labin <labin@10.10.1.15>
2017-05-18 21:34:42 -0300

    Lendo XML

--- LerXML/src/lerxml/LerXML.java
+++ LerXML/src/lerxml/LerXML.java
@@ -1,22 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lerxml;
 
-/**
- *
- * @author labin
- */
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
 public class LerXML {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Ola");
+    public static void main(String[] args) throws JDOMException, IOException {
+        File f = new File("C:\\Users\\labin\\Desktop\\clientes.xml");
+
+        SAXBuilder builder = new SAXBuilder();
+
+        Document doc = builder.build(f);
+
+        Element root = (Element) doc.getRootElement();
+
+        List cidades = root.getChildren();
+
+        Iterator i = cidades.iterator();
+
+        while (i.hasNext()) {
+            Element cidade = (Element) i.next();
+            System.out.println("Codigo: " + cidade.getAttributeValue("codigo"));
+            System.out.println("Nome: " + cidade.getAttributeValue("nome"));
+            System.out.println("UF: " + cidade.getAttributeValue("uf"));
+            System.out.println("\n");
+            List clientes = cidade.getChildren();
+            
+            Iterator j = clientes.iterator();
+            
+            while (j.hasNext()) {
+                Element cliente = (Element) j.next();
+                System.out.println("Matricula: " + cliente.getChildText("matricula"));
+                System.out.println("Nome: " + cliente.getChildText("nome"));
+                System.out.println("Data de Nascimento: " + cliente.getChildText("nascimento"));
+                System.out.println("\n");
+            }
+            
+            
+
+        }
     }
-    
+
 }
