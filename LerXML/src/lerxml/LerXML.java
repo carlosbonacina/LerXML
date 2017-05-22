2cfd529edf3fa483373df7cd54d0e58a9b2fecf0
labin <labin@10.10.1.9>
2017-05-22 20:31:50 -0300

    Versão Final

--- LerXML/src/lerxml/LerXML.java
+++ LerXML/src/lerxml/LerXML.java
@@ -2,6 +2,7 @@ package lerxml;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
 import org.jdom2.Document;
@@ -11,8 +12,8 @@ import org.jdom2.input.SAXBuilder;
 
 public class LerXML {
 
-    public static void main(String[] args) throws JDOMException, IOException {
-        File f = new File("C:\\Users\\labin\\Desktop\\clientes.xml");
+    public static void main(String[] args) throws JDOMException, IOException, ClassNotFoundException, SQLException {
+        File f = new File("C:\\Users\\labin\\Downloads\\clientes.xml");
 
         SAXBuilder builder = new SAXBuilder();
 
@@ -23,13 +24,17 @@ public class LerXML {
         List cidades = root.getChildren();
 
         Iterator i = cidades.iterator();
+        
+        InserirBD ins = new InserirBD();
 
         while (i.hasNext()) {
+            ins.abreConexao();
             Element cidade = (Element) i.next();
             System.out.println("Codigo: " + cidade.getAttributeValue("codigo"));
             System.out.println("Nome: " + cidade.getAttributeValue("nome"));
             System.out.println("UF: " + cidade.getAttributeValue("uf"));
             System.out.println("\n");
+            ins.insereCidade(Integer.parseInt(cidade.getAttributeValue("codigo")),cidade.getAttributeValue("nome") ,cidade.getAttributeValue("uf"));
             List clientes = cidade.getChildren();
             
             Iterator j = clientes.iterator();
@@ -40,8 +45,9 @@ public class LerXML {
                 System.out.println("Nome: " + cliente.getChildText("nome"));
                 System.out.println("Data de Nascimento: " + cliente.getChildText("nascimento"));
                 System.out.println("\n");
+                ins.insereCliente(Integer.parseInt(cliente.getChildText("matricula")),cliente.getChildText("nome") ,cliente.getChildText("nascimento") );
             }
-            
+            ins.fechaConexao();
             
 
         }
